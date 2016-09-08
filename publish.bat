979c151728a7744840df8b9c44f649ea47f2045b
monibm <monibm@LT5CG4504GC4.indexis.lan>
2016-09-08 09:14:50 +0200

    Updated doc generation

--- publish.bat
+++ publish.bat
@@ -1,3 +1,2 @@
 copy /Y D:\Users\monibm\git\forms-library\target\formslibrary-1.0.0-SNAPSHOT.jar c:\robot-tests\resources\lib\formslibrary.jar
-python -m robot.libdoc FormsLibrary C:\robot-tests\resources\lib\formslibrary.xml
-python -m robot.libdoc FormsLibrary C:\robot-tests\documentation\formslibrary.html
+copy /Y D:\Users\monibm\git\robotframework-formslibrary\doc\formslibrary.* c:\robot-tests\resources\lib\
