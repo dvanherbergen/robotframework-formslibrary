685fdf5053aabb9a104cde54c65b7694292ec2f0
monibm <monibm@LT5CG4504GC4.indexis.lan>
2016-10-27 16:22:44 +0200

    Publish in maven repository.

--- publish.bat
+++
@@ -1,3 +0,0 @@
-copy /Y D:\Users\monibm\git\robotframework-formslibrary\target\formslibrary-1.0.0-SNAPSHOT.jar c:\robot-tests\resources\lib\formslibrary.jar
-copy /Y D:\Users\monibm\git\robotframework-formslibrary\doc\formslibrary.xml c:\robot-tests\resources\lib\
-copy /Y D:\Users\monibm\git\robotframework-formslibrary\doc\formslibrary.html c:\robot-tests\documentation\
